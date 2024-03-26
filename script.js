d3deaa1936572982a21ea4eafe9b9c4c30d3ebc0
RiverCarley <river.carley@ocadu.ca>
2024-03-26 18:28:59 -0400

    fixed WORKING
    have to change image size and make reactive

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
-
     $(".speaker").click(function(){
+        // Check if the clicked speaker has the ID "speaker0"
+        if ($(this).attr("id") === "speaker0") {
+            return; // If so, do nothing and return
+        }
+        
         if ($(this).hasClass("active")) {
             $(".image-container img").click(function(event){
                 // Prevent the click event from bubbling up to the parent elements
